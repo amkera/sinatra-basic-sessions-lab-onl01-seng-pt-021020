c0923f8657b2b6988685ad4fae148b6d2392ad93
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-10 17:02:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do 
+    enable :sessions
+    set :session_secret, "lime_tea_with_honey"
+  end 
+  
+  get '/' do 
+    erb :'./views/index.erb'
+  end 
+  
+  post '/checkout' do 
+    @sessions = params["sessions"]
+  
+  
 end
